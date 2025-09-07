e806af0db85e39a0337172987a29a6ed8f23a23c
kimmybrindle <kimmybrindle@gmail.com>
2025-09-07 21:24:37 +1000

    added search functionality using api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function updateWeatherInfo(response) {
+  let temperatureElement = document.querySelector("#current-temp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchForCity(city) {
+  let apiKey = "5c04157d3b5bt2eeaa66db333fod0d64";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city-name");
-  cityElement.innerHTML = searchInput.value;
+
+  searchForCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
+searchForCity("Los Angeles");
